ab8ad5b04f196698ebe9a2bd46239a5601bac7c9
NetanelShaked <shaked5040>
2021-03-31 00:36:58 +0300

    Fixed filter by neighbors

--- Appendices/MargeFigiAndNeuroInfoData.py
+++ Appendices/MargeFigiAndNeuroInfoData.py
@@ -1,1 +1,48 @@
 import pandas as pd
+from scipy.spatial.distance import euclidean
+
+
+def find_closest_reagion(x_point, y_point, neuroinfo_as_dataframe):
+    distance = 50
+    neuroinfo_as_dataframe = pd.DataFrame(neuroinfo_as_dataframe)
+    neuroinfo_as_dataframe = neuroinfo_as_dataframe[
+        (neuroinfo_as_dataframe['x'] - distance < x_point) & (neuroinfo_as_dataframe['x'] + distance > x_point) & (
+                neuroinfo_as_dataframe['y'] - distance < -1 * y_point) & (
+                neuroinfo_as_dataframe['y'] + distance > -1 * y_point)]
+    closet_region = neuroinfo_as_dataframe.T.apply(
+        lambda row: [euclidean((row['x'], row['y']), (x_point, -y_point)), row['id'], row['hemisphere'], row['id name'],
+                     row['parent name'], row['region']]).T
+    if len(closet_region)==0:
+        return None
+    return closet_region[closet_region[0] == min(closet_region[0])]
+
+
+def marge_figi_neuroinfo(data_figi_path, data_neuroinfo_path):
+    figi_data = pd.read_csv(data_figi_path)
+    neuroinfo_data = pd.read_csv(data_neuroinfo_path)
+    new_neuroinfo_data=neuroinfo_data.copy()
+    distance = 3
+    for index, row in figi_data.iterrows():
+        x_point = row['XM']
+        y_point = row['YM']
+        is_exist = neuroinfo_data[
+            (neuroinfo_data['x'] - distance < x_point) & (neuroinfo_data['x'] + distance > x_point) & (
+                    neuroinfo_data['y'] - distance < -1 * y_point) & (
+                    neuroinfo_data['y'] + distance > -1 * y_point)]
+        if len(is_exist) == 0:
+            found_region = find_closest_reagion(x_point, y_point, neuroinfo_data)
+            if not type(found_region) == pd.DataFrame:
+                continue
+            new_neuroinfo_data = new_neuroinfo_data.append(
+                {'x': x_point, 'y': -1 * y_point, 'z': data_neuroinfo_path.split('\\')[-1][:-4], 'id': found_region[1].values[0],
+                 'hemisphere': found_region[2].values[0], 'id name': found_region[3].values[0], 'parent name': found_region[4].values[0],
+                 'region': found_region[5].values[0]}, ignore_index=True)
+    new_neuroinfo_data.to_csv('checking.csv',index=False)
+
+if __name__ == '__main__':
+    # csv = pd.read_csv(r'D:\Lab\Data_from_lab\N2-20210214T082519Z-012\N2\1h\csv files\separate files_2\-1255.csv')
+    # a = find_closest_reagion(10810, 10772, csv)
+    # print(a)
+    figi_path=r'C:\Users\shako\Desktop\Results.csv'
+    neuroinfo_path=r'D:\Lab\Data_from_lab\N2-20210214T082519Z-012\N2\1h\csv files\separate files_2\-1255.csv'
+    marge_figi_neuroinfo(figi_path,neuroinfo_path)
--- Filters/FilterNeunByNeighbors.py
+++ Filters/FilterNeunByNeighbors.py
@@ -13,23 +13,26 @@ def squere_info(image_path, csv_path):
 
     distance = 600
     cell_radius_in_pixel = 4
-    channel = 2
-    csv=pd.DataFrame(shuffle(csv)).reset_index().iloc[:min(len(csv),1000)]
-    for idx_main,row_main in csv.iterrows():
+    channel = 0
+    csv_iter = pd.DataFrame(shuffle(csv)).reset_index().iloc[:min(len(csv), 1000)]
+    for idx_main, row_main in csv_iter.iterrows():
         x_pixel = int(row_main['x'])
-        y_pixel = row_main['y']
-        relevant_csv = csv[(csv['y'] > y_pixel - distance) & (y_pixel + distance > csv['y'])]
-        relevant_csv = relevant_csv[(relevant_csv['x'] > x_pixel - distance) & (x_pixel + distance > relevant_csv['x'])]
+        y_pixel = int(row_main['y'])
+        relevant_csv = csv[(csv['y']>y_pixel-distance)&(csv['y']<y_pixel+distance)]
+        relevant_csv = relevant_csv[(relevant_csv['x']>x_pixel-distance)&(relevant_csv['x']<x_pixel+distance)]
         summarize_list = []
-        cell_locations=np.zeros((distance*2,distance*2))
+        cell_locations = np.zeros((distance * 2, distance * 2))
         y_pixel = abs(int(y_pixel))
         for idx, row in relevant_csv.iterrows():
             x_cell = int(row['x'])
             y_cell = abs(int(row['y']))
             summarize_list.append(image[y_cell - cell_radius_in_pixel:y_cell + cell_radius_in_pixel,
                                   x_cell - cell_radius_in_pixel:x_cell + cell_radius_in_pixel, channel].mean())
-            cell_locations[y_cell+distance-y_pixel-cell_radius_in_pixel:y_cell+distance-y_pixel+cell_radius_in_pixel,x_cell+distance-x_pixel-cell_radius_in_pixel:x_cell+distance-x_pixel+cell_radius_in_pixel]=1
+            cell_locations[
+            y_cell + distance - y_pixel - cell_radius_in_pixel:y_cell + distance - y_pixel + cell_radius_in_pixel,
+            x_cell + distance - x_pixel - cell_radius_in_pixel:x_cell + distance - x_pixel + cell_radius_in_pixel] = 1
         print("found: ", len(summarize_list))
+        print(len(cell_locations[cell_locations == 1]) / (cell_radius_in_pixel ** 2))
         plt.figure(figsize=(10, 10))
         plt.subplot(1, 2, 1)
         plt.hist(summarize_list, bins=int(max(summarize_list) + 1))
@@ -37,18 +40,19 @@ def squere_info(image_path, csv_path):
         plt.ylabel("number of cells")
         plt.title("Histogram: " + str(abs(y_pixel)) + "," + str(x_pixel) + " distance: " + str(distance))
         plt.subplot(1, 2, 2)
-        plt.title("found: "+ str(len(summarize_list)))
-        local_image=image[y_pixel - distance:y_pixel + distance,
-                   x_pixel - distance:x_pixel + distance, channel]
+        plt.title("found: " + str(len(summarize_list)))
+        local_image = image[y_pixel - distance:y_pixel + distance,
+                      x_pixel - distance:x_pixel + distance, channel]
         im=plt.imshow(local_image, cmap='gray')
         plt.imshow(cell_locations, cmap='copper', alpha=0.5)
+        # im = plt.imshow(cell_locations, cmap='copper')
         plt.colorbar(im)
-        plt.savefig(os.path.join(r'D:\Lab\4-3 talk_2',str(idx_main)+".jpg"))
+        plt.savefig(os.path.join(r'D:\Lab\4-3 talk_2', str(idx_main) + ".jpg"))
         plt.close()
     # plt.show()
 
 
 if __name__ == '__main__':
     image = r'D:\Lab\Data_from_lab\N2-20210214T082519Z-012\N2\1h\csv files\separate files_2\-1255.jp2'
-    csv = r'D:\Lab\Data_from_lab\N2-20210214T082519Z-012\N2\1h\csv files\separate files\-3000.csv'
+    csv = r'C:\Users\shako\PycharmProjects\EngramProject\Appendices\checking.csv'
     squere_info(image, csv)
